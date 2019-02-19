af5c9a01a5c31be924f5f8febbe0fad47f1ce003
Roman Messer <rms@gotocloud.net>
2019-02-19 22:51:31 +0100

    Generate askpass script in volatile /tmp
    Signed-off-by: Roman Messer <rms@gotocloud.net>

--- cmd/executor.sh
+++ cmd/executor.sh
@@ -7,7 +7,7 @@ cleanup() { rm $askpass; }
 trap cleanup 0 2 # cleanup on exit and kill
 
 mkdir -p $HOME/script
-askpass=$(mktemp --tmpdir="$HOME/script")
+askpass=$(mktemp)
 chmod +x $askpass
 
 cat << ASK > $askpass
