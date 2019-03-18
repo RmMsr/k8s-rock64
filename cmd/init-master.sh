b65be69341f51af6875d500815f0112a37e3ba41
Roman Messer <rms@gotocloud.net>
2019-03-18 20:40:38 +0100

    Make initialization of master idempotent
    Signed-off-by: Roman Messer <rms@gotocloud.net>

--- cmd/init-master.sh
+++ cmd/init-master.sh
@@ -19,10 +19,12 @@ fi
 
 echo === Init first kubernetes master
 
-# Using flannel networking
-exec_remote_su kubeadm init --pod-network-cidr=10.244.0.0/16
-exec_remote_su kubectl --kubeconfig /etc/kubernetes/admin.conf \
-    apply -f https://raw.githubusercontent.com/coreos/flannel/a70459be0084506e4ec919aa1c114638878db11b/Documentation/kube-flannel.yml
+if exec_remote ! test -f /etc/kubernetes/kubelet.conf; then
+    # Using flannel networking
+    exec_remote_su kubeadm init --pod-network-cidr=10.244.0.0/16
+    exec_remote_su kubectl --kubeconfig /etc/kubernetes/admin.conf \
+        apply -f https://raw.githubusercontent.com/coreos/flannel/a70459be0084506e4ec919aa1c114638878db11b/Documentation/kube-flannel.yml
+fi
 
 echo === Get cluster admin credentials
 
